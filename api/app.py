2d2ccb2f1c3f86f04aae5632c5a0e54545f851ce
snehomoy100@gmail.com <snehomoy100@gmail.com>
2021-12-12 16:50:45 +0530

    kubernetes client for python integrated

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ import sqlite3 # sqlite3 database as the database
 import yaml 
 from yaml import load, dump
 from yaml.loader import SafeLoader
-
+from kubernetes import client, config, watch
 
 from datetime import datetime # for the timestamp things of our db operation
 
@@ -20,22 +20,51 @@ cursor = sqliteConnection.cursor()
 print("Database created and Successfully Connected to SQLite")
 
 
+
+
+
+# Configs can be set in Configuration class directly or using helper utility
+config.load_kube_config()
+
+v1 = client.CoreV1Api()
+count = 10
+w = watch.Watch()
+for event in w.stream(v1.list_namespace, _request_timeout=60):
+    print("Event: %s %s" % (event['type'], event['object'].metadata.name))
+    count -= 1
+    if not count:
+        w.stop()
+
+print("Ended.")
+
+
+
+
+
+
+
+
+with open('fail.yml') as f:
+    json_data = yaml.load(f, Loader=SafeLoader)
+
+
 @app.route('/')
 def health_check():
   return 'Test Connection'
 
 # search api
-@app.route('/search', methods=['POST'])
-def search():
-  return jsonify(['my_series', 'another_series'])
+@app.route("/search", methods=["POST"])
+def grafana_search():
+    targets = find_targets()
+    return json.dumps(targets)
 
 # query api
 
-# @app.route('/query', methods=['POST'])
-# def query():
-#     req = request.get_json()
-#     data = 
-#     return jsonify(data)
+@app.route('/query', methods=['POST'])
+def query():
+    req = request.get_json()
+    data = json_data
+    return jsonify(data)
 
 
 if __name__ == "__main__": # running the app
